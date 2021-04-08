42659f10c5cda8a82e226a9d14e13230357eb781
anirbanmahapatra <anirbanmahapatra18@gmail.com>
2021-04-08 15:53:27 +0530

    Update AdmissionCommiteeMember.java

--- UniversityCourseAdmission/src/main/java/com/cg/mts/entities/AdmissionCommiteeMember.java
+++ UniversityCourseAdmission/src/main/java/com/cg/mts/entities/AdmissionCommiteeMember.java
@@ -3,48 +3,52 @@ package com.cg.mts.entities;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.Range;
 
 @Entity
 public class AdmissionCommiteeMember {
-	
-@Id
-@Column(name = "adminId")
-private int adminId;
-@Column(name = "adminName")
-private String adminName;
-@Column(name = "adminContact")
-private String adminContact;
 
-public AdmissionCommiteeMember() {
-	super();
-}
+	@Id
+	@Column(name = "adminId")
+	private int adminId;
 
-public int getAdminId() {
-	return adminId;
-}
+	@NotNull(message = "Name is required")
+	@Length(min = 5, max = 20, message = "name not between 5-20 letters")
+	@Column(name = "adminName")
+	private String adminName;
+	@NotNull(message = "Contact is required")
+	@Column(name = "adminContact")
+	private String adminContact;
 
-public void setAdminId(int adminId) {
-	this.adminId = adminId;
-}
+	public AdmissionCommiteeMember() {
+		super();
+	}
 
-public String getAdminName() {
-	return adminName;
-}
+	public int getAdminId() {
+		return adminId;
+	}
 
-public void setAdminName(String adminName) {
-	this.adminName = adminName;
-}
+	public void setAdminId(int adminId) {
+		this.adminId = adminId;
+	}
 
-public String getAdminContact() {
-	return adminContact;
-}
+	public String getAdminName() {
+		return adminName;
+	}
 
-public void setAdminContact(String adminContact) {
-	this.adminContact = adminContact;
-}
+	public void setAdminName(String adminName) {
+		this.adminName = adminName;
+	}
 
+	public String getAdminContact() {
+		return adminContact;
+	}
 
-
-
+	public void setAdminContact(String adminContact) {
+		this.adminContact = adminContact;
+	}
 
 }
