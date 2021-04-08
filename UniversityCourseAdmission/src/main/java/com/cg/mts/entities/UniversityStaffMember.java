1d00c865e04a38b492603e5e40c60e7fb95fdc76
SupratimSarkar19 <supratimsarkar99@gmail.com>
2021-04-08 15:37:27 +0530

    Update UniversityStaffMember.java

--- UniversityCourseAdmission/src/main/java/com/cg/mts/entities/UniversityStaffMember.java
+++ UniversityCourseAdmission/src/main/java/com/cg/mts/entities/UniversityStaffMember.java
@@ -4,5 +4,25 @@ public class UniversityStaffMember {
 private int staffId;
 private String password;
 private String role;
+public int getStaffId() {
+	return staffId;
+}
+public void setStaffId(int staffId) {
+	this.staffId = staffId;
+}
+public String getPassword() {
+	return password;
+}
+public void setPassword(String password) {
+	this.password = password;
+}
+public String getRole() {
+	return role;
+}
+public void setRole(String role) {
+	this.role = role;
+}
+
+
 
 }
