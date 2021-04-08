f5bc531817d0194aee7e0501a9172bc2e96a52fb
dasp7601 <dasp7601@gmail.com>
2021-04-08 16:54:10 +0530

    Create Address.java

---
+++ UniversityCourseAdmission/src/main/java/com/cg/mts/entities/Address.java
@@ -0,0 +1,49 @@
+package com.cg.mts.entities;
+
+public class Address {
+	private String city;
+	private String state;
+	private int pinCode;
+	private String roadName;
+	public Address() {
+		
+	}
+	public Address(String city, String state, int pinCode, String roadName) {
+		super();
+		this.city = city;
+		this.state = state;
+		this.pinCode = pinCode;
+		this.roadName = roadName;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getState() {
+		return state;
+	}
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public int getPinCode() {
+		return pinCode;
+	}
+	public void setPinCode(int pinCode) {
+		this.pinCode = pinCode;
+	}
+	public String getRoadName() {
+		return roadName;
+	}
+	public void setRoadName(String roadName) {
+		this.roadName = roadName;
+	}
+	@Override
+	public String toString() {
+		return "city=" + city + ", state=" + state + ", pinCode=" + pinCode
+				+ ", roadName=" + roadName;
+	} 
+	
+}
