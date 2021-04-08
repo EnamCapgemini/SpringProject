90d5d5042db3d3560308e4cc043ad94e0bc4788b
dasp7601 <dasp7601@gmail.com>
2021-04-08 16:11:10 +0530

    Update Applicant.java

--- UniversityCourseAdmission/src/main/java/com/cg/mts/entities/Applicant.java
+++ UniversityCourseAdmission/src/main/java/com/cg/mts/entities/Applicant.java
@@ -1,20 +1,16 @@
 package com.cg.mts.entities;
 
-import java.util.List;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
-import javax.validation.constraints.NotNull;
-
-import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import org.hibernate.validator.constraints.Length;
 
 @Entity
 @Table(name = "applicant")
-public class Applicant {
-
+public class Applicant {	
 	@Id
 	@Column(name = "APPLICANT_ID")
 	private String applicantId;
@@ -24,70 +20,12 @@ public class Applicant {
 	@NotNull(message="applicant name is required")
 	@Length(min=4,max=15,message="${name.length.error}")
 	private String applicantName;
-	
-
-	private String applicantId;
-	private String applicantName;
 
 	private String mobileNumber;
 	private String applicantDegree;
 	private int applicantGraduationPercent;
 	private Admission admission;
 	private AdmissionStatus status;
-
-
-	public String getApplicantId() {
-		return applicantId;
-	}
-
-	public void setApplicantId(String applicantId) {
-		this.applicantId = applicantId;
-	}
-
-	public String getApplicantName() {
-		return applicantName;
-	}
-
-	public void setApplicantName(String applicantName) {
-		this.applicantName = applicantName;
-	}
-
-	public String getMobileNumber() {
-		return mobileNumber;
-	}
-
-	public void setMobileNumber(String mobileNumber) {
-		this.mobileNumber = mobileNumber;
-	}
-
-	public String getApplicantDegree() {
-		return applicantDegree;
-	}
-
-	public void setApplicantDegree(String applicantDegree) {
-		this.applicantDegree = applicantDegree;
-	}
-
-	public int getApplicantGraduationPercent() {
-		return applicantGraduationPercent;
-	}
-
-	public void setApplicantGraduationPercent(int applicantGraduationPercent) {
-		this.applicantGraduationPercent = applicantGraduationPercent;
-	}
-
-	public Admission getAdmission() {
-		return admission;
-	}
-
-	public void setAdmission(Admission admission) {
-		this.admission = admission;
-	}
-
-	public AdmissionStatus getStatus() {
-		return status;
-	}
-	
 	public String getApplicantId() {
 		return applicantId;
 	}
@@ -130,5 +68,5 @@ public class Applicant {
 	public void setStatus(AdmissionStatus status) {
 		this.status = status;
 	}
-
-}
+	
+}	
