f7b477cc07ca85e8c5463a1d64fe19b5ac1724c3
anirbanmahapatra <anirbanmahapatra18@gmail.com>
2021-04-08 15:41:18 +0530

    Update AdmissionCommiteeMember.java

--- UniversityCourseAdmission/src/main/java/com/cg/mts/entities/AdmissionCommiteeMember.java
+++ UniversityCourseAdmission/src/main/java/com/cg/mts/entities/AdmissionCommiteeMember.java
@@ -1,7 +1,50 @@
 package com.cg.mts.entities;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
 public class AdmissionCommiteeMember {
+	
+@Id
+@Column(name = "adminId")
 private int adminId;
+@Column(name = "adminName")
 private String adminName;
+@Column(name = "adminContact")
 private String adminContact;
+
+public AdmissionCommiteeMember() {
+	super();
+}
+
+public int getAdminId() {
+	return adminId;
+}
+
+public void setAdminId(int adminId) {
+	this.adminId = adminId;
+}
+
+public String getAdminName() {
+	return adminName;
+}
+
+public void setAdminName(String adminName) {
+	this.adminName = adminName;
+}
+
+public String getAdminContact() {
+	return adminContact;
+}
+
+public void setAdminContact(String adminContact) {
+	this.adminContact = adminContact;
+}
+
+
+
+
+
 }
