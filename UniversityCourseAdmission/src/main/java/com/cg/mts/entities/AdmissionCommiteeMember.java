8a73c579d63240eab917fde46202d781ce4e1ce5
anirbanmahapatra <anirbanmahapatra18@gmail.com>
2021-04-08 16:27:45 +0530

    Update AdmissionCommiteeMember.java

--- UniversityCourseAdmission/src/main/java/com/cg/mts/entities/AdmissionCommiteeMember.java
+++ UniversityCourseAdmission/src/main/java/com/cg/mts/entities/AdmissionCommiteeMember.java
@@ -3,28 +3,42 @@ package com.cg.mts.entities;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 
 import org.hibernate.validator.constraints.Length;
-import org.hibernate.validator.constraints.Range;
+import org.hibernate.validator.constraints.NotEmpty;
 
+@SuppressWarnings("deprecation")
 @Entity
+@Table(name = "AdmissionCommiteeMember")
 public class AdmissionCommiteeMember {
 
 	@Id
 	@Column(name = "adminId")
 	private int adminId;
 
+	@NotEmpty(message = "Name must be to given")
 	@NotNull(message = "Name is required")
-	@Length(min = 5, max = 20, message = "name not between 5-20 letters")
+	@Length(min = 5, max = 20, message = "Name not between 5-20 letters")
 	@Column(name = "adminName")
 	private String adminName;
+
+	@NotEmpty(message = "Contact must be to given")
 	@NotNull(message = "Contact is required")
+	@Length(min = 2, max = 50, message = "Contact invalid")
 	@Column(name = "adminContact")
 	private String adminContact;
 
 	public AdmissionCommiteeMember() {
 		super();
+	}
+
+	public AdmissionCommiteeMember(int adminId, String adminName, String adminContact) {
+		super();
+		this.adminId = adminId;
+		this.adminName = adminName;
+		this.adminContact = adminContact;
 	}
 
 	public int getAdminId() {
