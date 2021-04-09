e4190bf81a92a7465693997941afce3e81018c62
milan2498 <milan24.nayak98@gmail.com>
2021-04-09 14:01:58 +0530

    Update CourseController.java

--- UniversityCourseAdmission/src/main/java/com/cg/mts/controller/CourseController.java
+++ UniversityCourseAdmission/src/main/java/com/cg/mts/controller/CourseController.java
@@ -5,10 +5,11 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.cg.mts.entities.Course;
 import com.cg.mts.exceptions.CourseNotFoundException;
 import com.cg.mts.service.CourseService;
 
@@ -21,12 +22,12 @@ public class CourseController {
 	CourseService service;
 	
 	@DeleteMapping("{cid}")
-	public String deleteCourse(@PathVariable("cid") int id) {
-		if(service.deleteCourseById(id))
-			return "data deleted";
-		else
-			throw new CourseNotFoundException("Delete Failed","Course with id to delete "+ id+"not found");
-	}
+public ResponseEntity<?> deleteCourseById( @RequestBody Course c) {
+		
+		service.deleteCourseById(c);
+		
+		return new ResponseEntity<>("Course deleted Successfully!", HttpStatus.OK);
+		
+	}}
 	
 
-}
