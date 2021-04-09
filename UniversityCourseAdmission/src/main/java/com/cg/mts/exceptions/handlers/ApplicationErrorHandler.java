0b5f84292d9a9d84e56af0c399039dfa43b5f0db
milan2498 <milan24.nayak98@gmail.com>
2021-04-09 13:54:36 +0530

    Update ApplicationErrorHandler.java

--- UniversityCourseAdmission/src/main/java/com/cg/mts/exceptions/handlers/ApplicationErrorHandler.java
+++ UniversityCourseAdmission/src/main/java/com/cg/mts/exceptions/handlers/ApplicationErrorHandler.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.cg.mts.exceptions.CourseNotFoundException;
 import com.cg.mts.exceptions.DuplicateStaffCredentialsException;
 import com.cg.mts.exceptions.StaffCredentialsNotFoundException;
 
@@ -36,5 +37,15 @@ public class ApplicationErrorHandler {
 		
 		return new ResponseEntity<>(errorBody, HttpStatus.BAD_REQUEST);
 	}
-
+	
+	@ExceptionHandler(CourseNotFoundException.class)
+	public ResponseEntity<?> handleDuplicate(CourseNotFoundException ex) {
+		Map<String, Object> errorBody = new LinkedHashMap<>();
+		
+		errorBody.put("error", "Not Found");
+		errorBody.put("timestamp", LocalDateTime.now());
+		errorBody.put("details", ex.getMessage());
+		
+		return new ResponseEntity<>(errorBody, HttpStatus.BAD_REQUEST);
+}
 }
