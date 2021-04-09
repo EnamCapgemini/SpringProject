4ba72a6fc24e451582c5d226d851d20470997839
anirbanmahapatra <anirbanmahapatra18@gmail.com>
2021-04-09 12:10:20 +0530

    Update data.sql

--- UniversityCourseAdmission/src/main/resources/data.sql
+++ UniversityCourseAdmission/src/main/resources/data.sql
@@ -7,3 +7,9 @@ insert into course values(330,"B.Tech CSE","4 years",LocalDate.now(),LocalDate.n
 insert into course values(320,"B.Tech ECE","4 years",LocalDate.now(),LocalDate.now().plus(4, ChronoUnit.YEARS),"700000");
 insert into course values(400,"B.A.","3 years",LocalDate.now(),LocalDate.now().plus(3, ChronoUnit.YEARS),"200000");
 insert into course values(500,"B.Ed","2 years",LocalDate.now(),LocalDate.now().plus(2, ChronoUnit.YEARS),"175000");
+
+insert into AdmissionCommiteeMember values(100, "Nagraj", "123@gmail.com");
+insert into AdmissionCommiteeMember values(400, "Dhruv", "765@gmail.com");
+insert into AdmissionCommiteeMember values(200, "Doga", "001@hotmail.com");
+insert into AdmissionCommiteeMember values(600, "Parmanu", "888@yandex.com");
+insert into AdmissionCommiteeMember values(300, "Gangadhar", "1580@gmail.com");
