0604d6b4d56447b647f1cb93dc8aaedf817ad78d
SupratimSarkar19 <supratimsarkar99@gmail.com>
2021-04-09 09:44:38 +0530

    Update UniversityStaffMember.java

--- UniversityCourseAdmission/src/main/java/com/cg/mts/entities/UniversityStaffMember.java
+++ UniversityCourseAdmission/src/main/java/com/cg/mts/entities/UniversityStaffMember.java
@@ -1,8 +1,14 @@
 package com.cg.mts.entities;
 
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
@@ -13,22 +19,29 @@ import org.hibernate.validator.constraints.NotEmpty;
 @SuppressWarnings("deprecation")
 @Entity
 @Table(name = "UniversityStaffs")
-public class UniversityStaffMember {
+public class UniversityStaffMember implements Serializable {
 
+	private static final long serialVersionUID = 1L;
+	
 	@Id
-	@Column(name = "staffId")
+	@Column(name = "STAFF_ID")
 	private int staffId;
 
 	@NotEmpty(message = "Password must have to given")
 	@NotNull(message = "Password can't be Null")
 	@Pattern(regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&-+=()])(?=\\\\S+$).{8, 20}$", message = "Password must contains one uppercase letter, one lowercase letter, one digit and a special character. Don't use any WhiteSapce.")
 	@Length(min = 8, max = 20,message = "Password length should be 8-20 characters")
+	@Column(name="STAFF_PASSWORD")
 	private String password;
 
 	@NotEmpty(message = "Staff Role must have to given")
 	@NotNull(message = "Staff Role can't be Null")
 	@Length(min = 2, max = 20)
+	@Column(name="STAFF_ROLE")
 	private String role;
+	
+	@OneToMany(mappedBy="courses",cascade=CascadeType.ALL)
+	private Set<Course> courses = new HashSet<>();
 
 	public UniversityStaffMember() {
 
