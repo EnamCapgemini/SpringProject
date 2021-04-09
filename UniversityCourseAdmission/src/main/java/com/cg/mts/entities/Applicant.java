60130545d868f08811a15e46897dac862fd94ca0
SupratimSarkar19 <supratimsarkar99@gmail.com>
2021-04-09 11:33:22 +0530

    Update Applicant.java

--- UniversityCourseAdmission/src/main/java/com/cg/mts/entities/Applicant.java
+++ UniversityCourseAdmission/src/main/java/com/cg/mts/entities/Applicant.java
@@ -17,79 +17,76 @@ import org.hibernate.validator.constraints.Email;
 import org.hibernate.validator.constraints.Length;
 
 @Entity
-@Table(name = "applicant")
+@Table(name = "APPLICANT")
 public class Applicant implements Serializable {
-	
+
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
 	@Column(name = "APPLICANT_ID")
 	private String applicantId;
-	
-	
-	@NotEmpty(message="applicant name should not be empty")
-	@NotNull(message="applicant name is required")
-	@Length(min=4,max=15,message="${name.length.error}")
-	@Column(name = "APPLICANT_NAME")
-	private String applicantName;
-	
-	
-	@NotEmpty(message="mobile number should not be empty")
-	@NotNull(message="mobile number is required")
-	@Length(min=10,max=10,message="${phone.length.error}")	
+
+	@Column(name = "APPLICANT_FIRSTNAME")
+	private String applicantFirstName;
+
+	@Column(name = "APPLICANT_LASTNAME")
+	private String applicantLastName;
+
+	@NotEmpty(message = "mobile number should not be empty")
+	@NotNull(message = "mobile number is required")
+	@Length(min = 10, max = 10, message = "${phone.length.error}")
 	@Column(name = "PH_NO")
 	private String mobileNumber;
-	
-	
-	@NotEmpty(message="degree should not be empty")
-	@NotNull(message="degree is required")
-	@Length(min=3,max=10,message="${degree.length.error}")
+
+	@NotEmpty(message = "degree should not be empty")
+	@NotNull(message = "degree is required")
+	@Length(min = 3, max = 10, message = "${degree.length.error}")
 	@Column(name = "DEGREE")
 	private String applicantDegree;
-	
-	
-	@NotEmpty(message="percent should not be empty")
-	@NotNull(message="percent is required")
+
+	@NotEmpty(message = "percent should not be empty")
+	@NotNull(message = "percent is required")
 	@Column(name = "GRAD_PER")
 	private int applicantGraduationPercent;
-	
+
 	@OneToOne
-	@JoinColumn(name="admission_id")
+	@JoinColumn(name = "ADMISSION_ID")
 	private Admission admission;
-	
+
 	@OneToOne
-	@JoinColumn(name="admission_status")
-	private AdmissionStatus status;
-	
+	@JoinColumn(name = "COURSE")
+	private Course course; ////////////////
+
 	@Column(name = "GENDER")
-	@NotEmpty(message="gender should not be empty")
+	@NotEmpty(message = "gender should not be empty")
 	private Gender gender;
-	
+
 	@OneToOne
-	@JoinColumn(name="address")
-	@NotEmpty(message="address should not be empty")
+	@JoinColumn(name = "ADDRESS")
+	@NotEmpty(message = "address should not be empty")
 	private Address address;
-	
+
 	@Column(name = "EMAIL_ID")
-	@NotEmpty(message="email should not be empty")
-	@NotNull(message="email is required")
+	@NotEmpty(message = "email should not be empty")
+	@NotNull(message = "email is required")
 	@Email(message = "email should be valid")
 	private String emailId;
-	
 
 	public Applicant() {
-		
+
 	}
-	
-	public Applicant(String applicantId,String applicantName, String mobileNumber, String applicantDegree,int applicantGraduationPercent,Admission admission, AdmissionStatus status,Gender gender, Address address, String emailId) {
+
+	public Applicant(String applicantId, String applicantFirstName, String applicantLastName, String mobileNumber,
+			String applicantDegree, int applicantGraduationPercent, Gender gender, Address address, String emailId) {
 		super();
 		this.applicantId = applicantId;
-		this.applicantName = applicantName;
+		this.applicantFirstName = applicantFirstName;
+		this.applicantLastName = applicantLastName;
 		this.mobileNumber = mobileNumber;
 		this.applicantDegree = applicantDegree;
 		this.applicantGraduationPercent = applicantGraduationPercent;
 		this.admission = admission;
-		this.status = status;
+		this.course = course;
 		this.gender = gender;
 		this.address = address;
 		this.emailId = emailId;
@@ -98,53 +95,65 @@ public class Applicant implements Serializable {
 	public String getApplicantId() {
 		return applicantId;
 	}
+
 	public void setApplicantId(String applicantId) {
 		this.applicantId = applicantId;
 	}
-	public String getApplicantName() {
-		return applicantName;
+
+	public String getApplicantFirstName() {
+		return applicantFirstName;
 	}
-	public void setApplicantName(String applicantName) {
-		this.applicantName = applicantName;
+
+	public void setApplicantFirstName(String applicantFirstName) {
+		this.applicantFirstName = applicantFirstName;
 	}
+
+	public String getApplicantLastName() {
+		return applicantLastName;
+	}
+
+	public void setApplicantLastName(String applicantLastName) {
+		this.applicantLastName = applicantLastName;
+	}
+
 	public String getMobileNumber() {
 		return mobileNumber;
 	}
+
 	public void setMobileNumber(String mobileNumber) {
 		this.mobileNumber = mobileNumber;
 	}
+
 	public String getApplicantDegree() {
 		return applicantDegree;
 	}
+
 	public void setApplicantDegree(String applicantDegree) {
 		this.applicantDegree = applicantDegree;
 	}
+
 	public int getApplicantGraduationPercent() {
 		return applicantGraduationPercent;
 	}
+
 	public void setApplicantGraduationPercent(int applicantGraduationPercent) {
 		this.applicantGraduationPercent = applicantGraduationPercent;
 	}
+
 	public Admission getAdmission() {
 		return admission;
 	}
+
 	public void setAdmission(Admission admission) {
 		this.admission = admission;
 	}
-	@Enumerated(EnumType.STRING)
-	public AdmissionStatus getStatus() {
-		return status;
-	}
-	public void setStatus(AdmissionStatus status) {
-		this.status = status;
-	}
-	@Enumerated(EnumType.STRING)
-	public Gender getGender() {
-		return gender;
+
+	public Course getCourse() {
+		return course;
 	}
 
-	public void setGender(Gender gender) {
-		this.gender = gender;
+	public void setCourse(Course course) {
+		this.course = course;
 	}
 
 	public Address getAddress() {
@@ -162,4 +171,14 @@ public class Applicant implements Serializable {
 	public void setEmailId(String emailId) {
 		this.emailId = emailId;
 	}
-}	
+
+	@Enumerated(EnumType.STRING)
+	public Gender getGender() {
+		return gender;
+	}
+
+	public void setGender(Gender gender) {
+		this.gender = gender;
+	}
+
+}
