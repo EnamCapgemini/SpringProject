56db974dda5c72e32132ad27aa5ba46d9a027c50
milan2498 <milan24.nayak98@gmail.com>
2021-04-09 13:54:43 +0530

    Create CourseRepository.java

---
+++ UniversityCourseAdmission/src/main/java/com/cg/mts/repository/CourseRepository.java
@@ -0,0 +1,14 @@
+package com.cg.mts.repository;
+
+
+import org.springframework.data.repository.CrudRepository;
+
+import org.springframework.stereotype.Repository;
+
+import com.cg.mts.entities.Course;
+
+
+
+public interface CourseRepository extends CrudRepository<Course, Integer>{
+	
+}
