3f15e4dc38535b9dd88e322bb7fca48b9498c42f
milan2498 <milan24.nayak98@gmail.com>
2021-04-09 11:57:45 +0530

    Update data.sql

---
+++ UniversityCourseAdmission/src/main/resources/data.sql
@@ -0,0 +1,9 @@
+insert into course values(100,"B.com","3 years",LocalDate.now(),LocalDate.now().plus(3, ChronoUnit.YEARS),"300000");
+insert into course values(200,"B.Sc Maths","3 years",LocalDate.now(),LocalDate.now().plus(3, ChronoUnit.YEARS),"350000");
+insert into course values(210,"B.Sc Physics","3 years",LocalDate.now(),LocalDate.now().plus(3, ChronoUnit.YEARS),"350000");
+insert into course values(220,"B.Sc Chemistry","3 years",LocalDate.now(),LocalDate.now().plus(3, ChronoUnit.YEARS),"350000");
+insert into course values(310,"B.Tech ME","4 years",LocalDate.now(),LocalDate.now().plus(4, ChronoUnit.YEARS),"700000");
+insert into course values(330,"B.Tech CSE","4 years",LocalDate.now(),LocalDate.now().plus(4, ChronoUnit.YEARS),"700000");
+insert into course values(320,"B.Tech ECE","4 years",LocalDate.now(),LocalDate.now().plus(4, ChronoUnit.YEARS),"700000");
+insert into course values(400,"B.A.","3 years",LocalDate.now(),LocalDate.now().plus(3, ChronoUnit.YEARS),"200000");
+insert into course values(500,"B.Ed","2 years",LocalDate.now(),LocalDate.now().plus(2, ChronoUnit.YEARS),"175000");
