ba69106f1bfc4a106903312522a8f0799bfc6f3d
dasp7601 <dasp7601@gmail.com>
2021-04-09 14:35:44 +0530

    Update StaffCredentialsRepository.java

--- UniversityCourseAdmission/src/main/java/com/cg/mts/repository/StaffCredentialsRepository.java
+++ UniversityCourseAdmission/src/main/java/com/cg/mts/repository/StaffCredentialsRepository.java
@@ -1,9 +1,10 @@
 package com.cg.mts.repository;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import com.cg.mts.entities.StaffCredentials;
-
+@Repository
 public interface StaffCredentialsRepository extends CrudRepository<StaffCredentials, Integer> {
 
 }
