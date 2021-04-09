badc4c512a10c5e6f1518d47d8c335e24287eeaf
anirbanmahapatra <anirbanmahapatra18@gmail.com>
2021-04-09 13:43:07 +0530

    AdmissionCommittee controller,1 exception, updated applicationerrorhandler, repository, service class

--- UniversityCourseAdmission/src/main/java/com/cg/mts/exceptions/handlers/ApplicationErrorHandler.java
+++ UniversityCourseAdmission/src/main/java/com/cg/mts/exceptions/handlers/ApplicationErrorHandler.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.cg.mts.exceptions.DuplicateAdmissionCommiteeMemberException;
 import com.cg.mts.exceptions.DuplicateStaffCredentialsException;
 import com.cg.mts.exceptions.StaffCredentialsNotFoundException;
 
@@ -31,6 +32,17 @@ public class ApplicationErrorHandler {
 		Map<String, Object> errorBody = new LinkedHashMap<>();
 		
 		errorBody.put("error", "Not Found");
+		errorBody.put("timestamp", LocalDateTime.now());
+		errorBody.put("details", ex.getMessage());
+		
+		return new ResponseEntity<>(errorBody, HttpStatus.BAD_REQUEST);
+	}
+	
+	@ExceptionHandler(DuplicateAdmissionCommiteeMemberException.class)
+	public ResponseEntity<?> handleDuplicateEntity(DuplicateAdmissionCommiteeMemberException ex) {
+		Map<String, Object> errorBody = new LinkedHashMap<>();
+		
+		errorBody.put("error", "Creation Failed");
 		errorBody.put("timestamp", LocalDateTime.now());
 		errorBody.put("details", ex.getMessage());
 		
---
+++ UniversityCourseAdmission/src/main/java/com/cg/mts/repository/AdmissionCommiteeMemberRepository.java
@@ -0,0 +1,9 @@
+package com.cg.mts.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.cg.mts.entities.AdmissionCommiteeMember;
+
+public interface AdmissionCommiteeMemberRepository extends CrudRepository<AdmissionCommiteeMember, Integer>{
+
+}
