8cd192fead490f36059c09a16037ea34b7ae1d61
dasp7601 <dasp7601@gmail.com>
2021-04-09 14:14:19 +0530

    Create ApplicantRepository.java

---
+++ UniversityCourseAdmission/src/main/java/com/cg/mts/repository/ApplicantRepository.java
@@ -0,0 +1,9 @@
+package com.cg.mts.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.cg.mts.entities.Applicant;
+
+public interface ApplicantRepository extends CrudRepository<Applicant, Integer> {
+
+}
