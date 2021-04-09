fe0bd9a267db54b44f1a52c3c865c221f07e3714
milan2498 <milan24.nayak98@gmail.com>
2021-04-09 13:54:39 +0530

    Create CourseService.java

---
+++ UniversityCourseAdmission/src/main/java/com/cg/mts/service/CourseService.java
@@ -0,0 +1,24 @@
+package com.cg.mts.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.cg.mts.entities.Course;
+import com.cg.mts.exceptions.CourseNotFoundException;
+import com.cg.mts.repository.CourseRepository;
+
+@Service
+public class CourseService {
+	@Autowired
+	CourseRepository repository;
+	
+	public boolean deleteCourseById(Course c) {
+		if(repository.existsById(c.getCourseId())) {
+			repository.deleteById(c.getCourseId()); 
+		return true;}
+		else
+			throw new CourseNotFoundException("course with id "+c.getCourseId()+"doesnt exist");
+	}
+
+}
+
