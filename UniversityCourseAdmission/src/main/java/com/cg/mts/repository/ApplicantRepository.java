988f6cd818f85c189ff7944ce2cb71e2176bd132
dasp7601 <dasp7601@gmail.com>
2021-04-09 14:35:52 +0530

    Update ApplicantRepository.java

--- UniversityCourseAdmission/src/main/java/com/cg/mts/repository/ApplicantRepository.java
+++ UniversityCourseAdmission/src/main/java/com/cg/mts/repository/ApplicantRepository.java
@@ -1,9 +1,10 @@
 package com.cg.mts.repository;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import com.cg.mts.entities.Applicant;
-
+@Repository
 public interface ApplicantRepository extends CrudRepository<Applicant, Integer> {
 
 }
