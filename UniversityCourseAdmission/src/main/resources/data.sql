e8e4c67cafd07888aa175723058bedc0ef146912
SupratimSarkar19 <supratimsarkar99@gmail.com>
2021-04-09 16:13:32 +0530

    Update data.sql

--- UniversityCourseAdmission/src/main/resources/data.sql
+++ UniversityCourseAdmission/src/main/resources/data.sql
@@ -8,11 +8,11 @@ insert into course values(320,"B.Tech ECE","4 years",LocalDate.now(),LocalDate.n
 insert into course values(400,"B.A.","3 years",LocalDate.now(),LocalDate.now().plus(3, ChronoUnit.YEARS),"200000");
 insert into course values(500,"B.Ed","2 years",LocalDate.now(),LocalDate.now().plus(2, ChronoUnit.YEARS),"175000");
 
-insert into AdmissionCommiteeMember values(100, "Nagraj", "123@gmail.com");
-insert into AdmissionCommiteeMember values(400, "Dhruv", "765@gmail.com");
-insert into AdmissionCommiteeMember values(200, "Doga", "001@hotmail.com");
-insert into AdmissionCommiteeMember values(600, "Parmanu", "888@yandex.com");
-insert into AdmissionCommiteeMember values(300, "Gangadhar", "1580@gmail.com");
+insert into AdmissionCommiteeMember values(100, "Nagraj", "123@gmail.com",10025);
+insert into AdmissionCommiteeMember values(400, "Dhruv", "765@gmail.com",10026);
+insert into AdmissionCommiteeMember values(200, "Doga", "001@hotmail.com",10027);
+insert into AdmissionCommiteeMember values(600, "Parmanu", "888@yandex.com",10028);
+insert into AdmissionCommiteeMember values(300, "Gangadhar", "1580@gmail.com",10030);
 
 
 insert into UniversityStaffs values(10023,"Abc54tc_98","Coordinator");
@@ -20,8 +20,8 @@ insert into UniversityStaffs values(10024,"a_ctBG6709","Head");
 insert into UniversityStaffs values(10025,"Admission_21","Admission Committee");
 insert into UniversityStaffs values(10026,"committeeM_2M","Admission Committee");
 insert into UniversityStaffs values(10027,"Faculty_56","Admission Committee");
-insert into UniversityStaffs values(10026,"committee35_g","Admission Committee");
-insert into UniversityStaffs values(10026,"com56_rteu78","Admission Committee");
+insert into UniversityStaffs values(10028,"committee35_g","Admission Committee");
+insert into UniversityStaffs values(10030,"com56_rteu78","Admission Committee");
 
 insert into StaffCredentials values(101,"pass@123");
 insert into StaffCredentials values(105,"pass@456");
