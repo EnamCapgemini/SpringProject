15b972e284ea46e52151bac63019ed31b70306d4
ayush5691 <hackib020@gmail.com>
2021-04-15 18:58:03 +0530

    Update TestAdmission.java

--- UniversityCourseAdmission/src/test/java/com/cg/mts/tests/TestAdmission.java
+++ UniversityCourseAdmission/src/test/java/com/cg/mts/tests/TestAdmission.java
@@ -17,36 +17,36 @@ import com.cg.mts.service.AdmissionService;
 
 @SpringBootTest
 class TestAdmission {
-	
+
 	@Autowired
 	private AdmissionService service;
-	
+
 	@MockBean
 	private IAdmissionRepository repository;
 
 	@Test
 	public void addAdmission() {
-		Admission a = new Admission(100,1000,LocalDate.now(),AdmissionStatus.PENDING);
+		Admission a = new Admission(100, 1000, LocalDate.now(), AdmissionStatus.PENDING);
 		when(repository.save(a)).thenReturn(a);
 		assertEquals(a, repository.save(a));
 	}
-	
+
 	@Test
 	public void updateAdmission() {
-		Admission a = new Admission(100,1000,LocalDate.now(),AdmissionStatus.PENDING);
+		Admission a = new Admission(100, 1000, LocalDate.now(), AdmissionStatus.PENDING);
 		when(repository.save(a)).thenReturn(a);
-<<<<<<< Updated upstream
-		a = new Admission(100,100,LocalDate.now(),AdmissionStatus.APPLIED);
-=======
-		a = new Admission(100,2000,LocalDate.now(),AdmissionStatus.APPLIED);
->>>>>>> Stashed changes
-		assertNotEquals(a,repository.save(a));
+
+		a = new Admission(100, 100, LocalDate.now(), AdmissionStatus.APPLIED);
+
+		a = new Admission(100, 2000, LocalDate.now(), AdmissionStatus.APPLIED);
+
+		assertNotEquals(a, repository.save(a));
 	}
-	
+
 	@Test
 	public void deleteAdmission() {
-		Admission a = new Admission(100,1000,LocalDate.now(),AdmissionStatus.PENDING);
-		assertNotEquals(a,service.deleteAdmission(100));
+		Admission a = new Admission(100, 1000, LocalDate.now(), AdmissionStatus.PENDING);
+		assertNotEquals(a, service.deleteAdmission(100));
 	}
-	
+
 }
