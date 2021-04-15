ee86d20670ea7f2924ac44a67a02ee4f8ea2ebb4
ayush5691 <hackib020@gmail.com>
2021-04-15 18:08:07 +0530

    Update IAdmissionService.java

--- UniversityCourseAdmission/src/main/java/com/cg/mts/service/IAdmissionService.java
+++ UniversityCourseAdmission/src/main/java/com/cg/mts/service/IAdmissionService.java
@@ -5,12 +5,17 @@ import java.util.List;
 import com.cg.mts.entities.Admission;
 
 public interface IAdmissionService {
-	
+
 	void addAdmission(Admission a);
+
 	boolean updateAdmission(Admission a);
+
 	boolean deleteAdmission(int id);
+
 	Admission getAdmission(int id);
+
 	List<Admission> showAllByDate(String adDate);
+
 	List<Admission> showAllByCourseId(int id);
 
 }
