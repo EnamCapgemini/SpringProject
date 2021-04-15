123bb788a52a2d30f6e135ba3767b0744a3b1a11
ayush5691 <hackib020@gmail.com>
2021-04-15 20:40:26 +0530

    Update IAdmissionRepository.java

--- UniversityCourseAdmission/src/main/java/com/cg/mts/repository/IAdmissionRepository.java
+++ UniversityCourseAdmission/src/main/java/com/cg/mts/repository/IAdmissionRepository.java
@@ -1,11 +1,22 @@
 package com.cg.mts.repository;
 
-import org.springframework.data.repository.CrudRepository;
+import java.time.LocalDate;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.cg.mts.entities.Admission;
+import com.cg.mts.entities.AdmissionStatus;
 
 @Repository
-public interface IAdmissionRepository extends CrudRepository<Admission, Integer> {
+public interface IAdmissionRepository extends JpaRepository<Admission, Integer> {
+
+	@Modifying
+	@Transactional
+	@Query("update Admission a set a.admissionDate=?1,a.status=?2 where a.admissionId=?3")
+	void updateAdmissionTable(LocalDate ld, AdmissionStatus status, int aid);
 
 }
