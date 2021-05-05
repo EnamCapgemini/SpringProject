b3dd87d0d93aded64b11b594073d06d790cf8529
SupratimSarkar19 <supratimsarkar99@gmail.com>
2021-05-05 10:10:01 +0530

    cross origin

--- UniversityCourseAdmission/src/main/java/com/cg/mts/controller/UniversityStaffController.java
+++ UniversityCourseAdmission/src/main/java/com/cg/mts/controller/UniversityStaffController.java
@@ -17,7 +17,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 //import org.springframework.web.bind.annotation.PatchMapping;
@@ -41,6 +41,7 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 @RestController
 @RequestMapping("/UniversityStaffs")
 @JsonIgnoreProperties("Password")
+@CrossOrigin(origins = "http://localhost:4200")
 public class UniversityStaffController {
 
 	@Autowired
@@ -97,7 +98,7 @@ public class UniversityStaffController {
 	@DeleteMapping("/{staffId}")
 	public String removeStaff(@PathVariable("staffId") int sId) {
 		if (universityService.removeStaff(sId))
-			return "Staff Data Deleted Succesfully";
+			return "Staff Data with id "+sId+" Deleted Succesfully";
 		else
 			throw new DataNotFoundException("Delete", "Staff with id " + sId + " not found");
 	}
